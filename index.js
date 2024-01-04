16acf920ffad96f245cc0bbd37f26f6021713cc1
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-04 14:16:39 -0700

    connected to express en mysql2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const UserInput = require("./lib/user-input");
+const { addDepartment } = require("./lib/document"); //helper functions
 
 // DATA
 const userInput = new UserInput();
@@ -7,7 +8,10 @@ const userInput = new UserInput();
 async function getUsersChoice() {
   const choice = await userInput.getInput();
 
-  console.log(choice);
+  if (choice == "Add Department") {
+    const departmentName = await userInput.addDepartment();
+    console.log(departmentName);
+  }
 }
 
 getUsersChoice();
