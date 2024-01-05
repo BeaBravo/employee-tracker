6bf07e663cdbec15d02b2fbdba461cd834cbbd5b
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-04 17:01:16 -0700

    started index.js again with pseudo code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // DEPENDENCIES
-const UserInput = require("./lib/user-input");
-// const { addDepartment } = require("./lib/document"); //helper functions
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 // DATA
@@ -17,16 +16,7 @@ const db = mysql.createConnection({
 });
 
 async function getUsersChoice() {
-  const choice = await userInput.getInput();
-
-  if (choice == "Add Department") {
-    const departmentName = await userInput.addDepartment();
-    addDepartment(departmentName);
-    return;
-  } else if (choice == "View All Departments") {
-    viewAllDepartments();
-    return;
-  }
+  //getUsersChoice is to prompt the option menu
 }
 
 function addDepartment(department) {
@@ -55,5 +45,41 @@ function viewAllDepartments() {
   );
 }
 
+function addRole() {
+  //prompts to ask the name of the role
+  //asks for salary of the role
+  //asks which department the role belongs to?
+  //will need to call viewAllDepartments to get the most updated list
+}
+
+function addEmployee() {
+  //asks for first name
+  //asks for last name
+  //asks for the role
+  //asks for the employees manager
+}
+
+function viewAllEmployees() {}
+
+function viewAllRoles() {}
+
+function updateEmployeeRole() {
+  //asks for the employees name from a list
+  //asks for the role from a list
+}
+
+async function init() {
+  // const choice;
+
+  if (choice == "Add Department") {
+    const departmentName = await userInput.addDepartment();
+    addDepartment(departmentName);
+    return;
+  } else if (choice == "View All Departments") {
+    viewAllDepartments();
+    return;
+  }
+}
+
 //initialize
-getUsersChoice();
+init();
